86c931747d48abf6021881b62f37694945cf7dbb
linhgithub23 <89020917+linhgithub23@users.noreply.github.com>
2022-01-12 23:13:59 +0700

    Array and pointer

--- codecpp/helloWorld.cpp
+++ codecpp/helloWorld.cpp
@@ -3,17 +3,6 @@ using namespace std;
 const int MAX = 3;
 int main(){
     int var[MAX] = {10,100,200};
-    int *ptr;
-
-    //let us have array address in pointer
-    ptr = var;
-
-    for(int i = 0; i<MAX; i++){
-        cout<<"Address of var["<<i<<"] = "<<ptr<<endl;
-        cout<<"Value of var["<<i<<"] = "<<*ptr<<endl;
-
-        //point to the next location
-        ptr++;
-    }
+    cout<<"Hello world";
     return 0;
 }
