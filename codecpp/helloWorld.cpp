897e362ff01d128a9fa55dbacb954d06cbe3e27a
linhgithub23 <89020917+linhgithub23@users.noreply.github.com>
2022-01-16 09:43:10 +0700

    New update

--- codecpp/helloWorld.cpp
+++ codecpp/helloWorld.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello world";
-    return 0;
+    
+    int array[5] = {1,2,3,4,5 };
+    for(int i = 0; i<6 ; i++){
+        array[i]+=1;
+    }
+    int min = array[0];
+    for(int i = 0; i<6; i++){
+        if(array[i] > min){
+            min = array[i];
+        }
+    }
+    cout<<min;
 }
